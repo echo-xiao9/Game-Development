0cbf25c8d58cb02e677f5186676eeb2e03e019a4
echo-xiao9 <819601193@qq.com>
2022-02-25 10:08:43 +0800

    hw2 left ui

---
+++ hw2 2D Game with Physics/2d-physics/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  Rigidbody2D rb;
+  public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        // print("update");
+    }
+
+    void Move(){
+      float xInput = Input.GetAxisRaw("Horizontal");
+      rb.velocity = new Vector2(xInput*speed, rb.velocity.y);
+      // diff with that?
+      // transform.position = new Vector3(transform.position.x+speed*Time.deltaTime, transform.position.y, transform.position.z);
+      if(xInput !=0)
+        transform.localScale = new Vector3(xInput, 1,1);
+    }
+
+    public void Die(){
+      print("Die");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Spike"))
+        {
+        Die(); 
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    { 
+      if(other.gameObject.CompareTag("Spike"))
+        {
+          Die(); 
+        }
+    }
+}
---
+++ hw2 2D Game with Physics/2d-physics/Assets/Scripts/Spawner.cs
@@ -0,0 +1,46 @@
+﻿// using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public List<GameObject> platforms;
+    //定义多少时间生成平台
+    public float spawnTime;
+    //record how much time passed
+    private float countTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countTime += Time.deltaTime;
+        // print(countTime);
+        if(countTime >=spawnTime){
+          SpawnPlatform();
+          // can calculate time again
+          countTime = 0;
+        }
+    }
+
+    public void SpawnPlatform(){
+      // the posiion is the same at the buttomline
+      Vector3 spawnPosition = transform.position;
+      // the range of x
+      spawnPosition.x = Random.Range(-3.5f, 3.5f);
+      // the kind of the platform
+      int index = Random.Range(0, platforms.Count);
+      print("generate new plateform:"+index);
+      // use Instantiate to generate platforms, angle: Quaternion.identity
+      GameObject go = Instantiate(platforms[index], spawnPosition,
+      Quaternion.identity);
+      go.transform.SetParent(this.gameObject.transform);
+    }
+
+
+}
