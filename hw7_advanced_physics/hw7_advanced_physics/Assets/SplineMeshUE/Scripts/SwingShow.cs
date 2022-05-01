acca3009ab42f718e02155f57ca5c71acd804e8b
echo-xiao9 <819601193@qq.com>
2022-05-01 14:17:28 +0800

    finish hw7 part1:hair basic

---
+++ hw7_advanced_physics/hw7_advanced_physics/Assets/SplineMeshUE/Scripts/SwingShow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwingShow : MonoBehaviour
+{
+    public UESpline spline;
+    public float totalTime = 2.0f;
+
+    // Use this for initialization
+    void Start()
+    {   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float ctime = Time.time % totalTime / totalTime;
+        var pos = spline.GetPositionLerp(ctime);
+        transform.position = pos;
+        //spline.
+    }
+}
